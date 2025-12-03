8cc23d7055fbbe0e758789fc33cbb16df3c0d9cd
uhs <uelhamoumi@gmail.com>
2025-12-03 13:53:44 +0100

    Correcció Pràctica Final UD2

--- Pràctica Final UD2/Activitat-11.py
+++ Pràctica Final UD2/Activitat-11.py
@@ -12,11 +12,11 @@ try:
     resta= num1 - num2
     multiplicacio= num1 * num2 
     divisio= num1 / num2
-    print(f"La suma de {num1} + {num2} es igual a {suma}")
-    print(f"La resta de  {num1} - {num2} es igual a {resta}")
-    print(f"La multiplicació de {num1} * {num2} es igual a {multiplicacio}")
-    print(f"La divisió de {num1} / {num2} es igual a {divisio}")
+    logging.debug(f"La suma de {num1} + {num2} es igual a {suma}")
+    logging.debug(f"La resta de  {num1} - {num2} es igual a {resta}")
+    logging.debug(f"La multiplicació de {num1} * {num2} es igual a {multiplicacio}")
+    logging.debug(f"La divisió de {num1} / {num2} es igual a {divisio}")
 except ZeroDivisionError:
-    print("El número 0 no es pot dividir, proba un altre")
+    logging.debug("El número 0 no es pot dividir, proba un altre")
 except ValueError:
-    print("Soles pots posar números enters")
+    logging.debug("Soles pots posar números enters")
